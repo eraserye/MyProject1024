816a7fe7e93f1599924230f4aad4f6c65ca9d916
eraserye <1483410733@qq.com>
2023-12-17 11:27:51 +0800

    s

---
+++ Source/MyProject1024/MyProject1024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyProject1024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProject1024, "MyProject1024" );
---
+++ Source/MyProject1024/MyProject1024GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProject1024GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT1024_API AMyProject1024GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
